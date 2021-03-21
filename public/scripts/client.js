7e13c3d836edc36c622fb5d37ae08f25bbcc9cc6
Paul Ladd <vlendle@gmail.com>
2021-03-21 21:11:31 +0000

    Added logic to submit a username locally

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,12 +1,19 @@
 const socket = io();
 const game = document.getElementById('game');
 const refresh = document.getElementById('refresh');
+const login = document.getElementById('loginForm')
+const nameField = document.getElementById('inputName');
 
 refresh.addEventListener('submit', (event) => {
   event.preventDefault();
   game.innerHTML = '<h1>HOWDY</h1>';
 })
 
-
+login.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (nameField.value) {
+    game.innerHTML = `<p>${nameField.value}</p>`;
+  }
+})
 
 game.innerHTML = '<strong>hi</strong>';

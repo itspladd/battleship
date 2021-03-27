5820395af2052266f928c686b7a22b6827007d58
Paul Ladd <vlendle@gmail.com>
2021-03-27 14:03:54 +0000

    Remove validation that is now performed by datahelpers. Add random ID generation.

--- server/scripts/serverHelpers.js
+++ server/scripts/serverHelpers.js
@@ -1,8 +1,28 @@
-const validateUser = (user) => {
-  console.log('TODO: Validate users properly.');
-  return true;
+const randomUserId = () => {
+  return `p${randomString(8)}`;
 }
 
+// Generates a random integer between two values.
+const randInt = (min, max) => {
+  const range = (max - min) + 1;
+  return Math.floor((Math.random() * range) + min);
+};
+
+// Creates a string of random alphanumeric (upper and lowercase) characters
+// Length can be any length, default is 6
+const randomString = (length = 6) => {
+  let str = "";
+
+  while (str.length < length) {
+    let code = randInt(48, 122);
+    if (!(code > 57 && code < 65) && !(code > 90 && code < 97)) {
+      str += String.fromCharCode(code);
+    }
+  }
+
+  return str;
+};
+
 module.exports = {
-  validateUser
+  randomUserId,
 };

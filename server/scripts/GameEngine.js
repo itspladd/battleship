c6396e673eb9b9d93b29d82e90bdf05326fc3361
Paul Ladd <vlendle@gmail.com>
2021-03-31 02:31:44 +0000

    Remove waitForPlayers since we will check for enough players on player join

--- server/scripts/GameEngine.js
+++ server/scripts/GameEngine.js
@@ -46,21 +46,9 @@ class GameEngine {
     this.db.getPlayer(hostID)
     .then(player => addPlayer(player, socket))
     .catch(err => this.db.logError(err));
-    // Wait for players, then
-    // Get both players to set up boards, then
-    // Choose first player, then
-    // Start game loop
-      // Player sends move, then
-      // Validate the move, then
-      // Check for game end, then
-        // Go to postgame if game is over
-      // Update the board, then
-      // Switch players, then
-      // Back to start
+
+    // Get the board initialized
     this.board = this.setupBoard(this.rules);
-    this.waitForPlayers()
-    .then(res => console.log(res))
-    .catch(rej => console.log(rej));
   }
 
   addPlayer(player, socket) {
@@ -81,26 +69,6 @@ class GameEngine {
       
     });
   }
-
-  // Will poll the number of tracked players and start a game when there's enough. 
-  waitForPlayers() {
-    return new Promise( (resolve, reject) => {
-      let polls = 0;
-      console.log('Waiting for players...');
-      const wait = setInterval( () => {
-        polls++;
-        if (this.enoughPlayers()) {
-          clearInterval(wait);
-          resolve('Enough players!');
-          return;
-        }
-        if (polls === this.timeoutDuration) {
-          clearInterval(wait);
-          reject('Waiting for players timed out.');
-        }
-      }, this.pollFrequency);
-    });
-  };
 
   enoughPlayers() {
     return this.playerNames.length === this.rules.players;
